22246c7fe95e8c3828d0f8bf7d11b0d521e34c35
Timothy Saudners <timsaunders88@gmail.com>
2020-09-03 17:16:41 +0100

    03/09/20 17:16 added GameGrid.vue with basic structural content

---
+++ client/src/components/GameGrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="grid">
+        <grid-cell v-for="(cell, key) in player.grid" :key="key" :cell="cell" :id="cell.coords"/>
+    </section>
+</template>
+
+<script>
+import GridCell from './GridCell.vue';
+
+export default {
+    name: 'game-grid',
+    props: ['player'],
+    components: {'grid-cell': GridCell}
+};
+</script>
+
+<style>
+    .grid{
+        display: grid;
+    }
+</style>
