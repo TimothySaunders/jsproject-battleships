f56fde397e78906494cdbbcd806dd9f66f45796b
marekboro <marek.borowicz83@gmail.com>
2020-09-03 17:18:30 +0100

    added get,add and update Game to the gameServices

---
+++ client/services/GameService.js
@@ -0,0 +1,33 @@
+const baseURL = "http://localhost:3000/api/battelships/";
+
+export default {
+        getGame(){
+            return fetch(baseURL).then(res => res.json())
+        },
+        // getAGameById(id){                //for LOAD a game? 
+        //     return fetch(baseURL+id).then(res => res.json())
+        // },
+
+        addGame(payload){
+            return fetch(baseURL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payload)
+            })
+        },
+        updateGame(_id, payload) {
+            return fetch(baseURL+id, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(payload)
+            })
+            .then(res => res.json());
+        }
+
+
+        
+
+
+
+
+}
