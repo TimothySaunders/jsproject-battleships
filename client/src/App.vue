4e818bb0117162065b691afdf1e74a2b14b6232c
marekboro <marek.borowicz83@gmail.com>
2020-09-06 18:36:38 +0100

    6/9/2020 18:37 addPotentialTargets now removes duplicates and out of bound targets

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,11 +10,10 @@
 <script>
 import GameGrid from "./components/GameGrid.vue";
 import GameService from "./services/GameService.js";
-import GoodBrain from "./services/GoodBrain.js";           //! AI
-  
-const themessage = GoodBrain.printSomething();             //! AI  
-console.log(themessage);                                   //! AI
+import GoodBrain from "./services/GoodBrain.js"; //! AI
 
+// const themessage = GoodBrain.printSomething();             //! AI
+// console.log(themessage);                                   //! AI
 
 import { eventBus } from "@/main.js";
 
@@ -37,7 +36,7 @@ export default {
   methods: {
     checkIfHit(cell) {
       let target = this.getTarget();
-      const key = 8 * cell.coords.x + cell.coords.y; 
+      const key = 8 * cell.coords.x + cell.coords.y;
       let isHit;
       let shipToSinkIndex;
       let shipToSink;
@@ -64,7 +63,7 @@ export default {
       } else {
         target.grid[key].state = "miss";
       }
-      this.registerHitToMemory(cell);
+      this.registerHitToMemory(cell); //! used for AI
       this.turns += 1;
       this.switchPlayer();
     },
@@ -74,7 +73,8 @@ export default {
         ? this.playerTwo
         : this.playerOne;
     },
-    getShooter(){
+    getShooter() {
+      //! used for AI
       return this.playerOne.playerName === this.playerTurn
         ? this.playerOne
         : this.playerTwo;
@@ -100,14 +100,67 @@ export default {
     checkIfAllSunk(player) {
       return player.ships.notSunk.length === player.ships.sunk.length;
     },
-    registerHitToMemory( cell){            //! AI
+    registerHitToMemory(cell) {
+      //! used for AI
       let shooter = this.getShooter();
-      shooter.brain.hitHistory.push(cell)
-    },          
-    establishTargets(){                   //! AI
-      
+      let length = shooter.brain.hitHistory.push(cell); // Length here used to test in console log below:
+
+      console.log(
+        `${shooter.playerName} registered: x:${
+          shooter.brain.hitHistory[length - 1].coords.x
+        } y:${shooter.brain.hitHistory[length - 1].coords.y} state: ${
+          shooter.brain.hitHistory[length - 1].state
+        }`
+      );
+      // this.setPotentialTargets(shooter,cell);
+      this.addPotentialTargets(shooter, cell);
     },
-    
+    addPotentialTargets(a_shooter, a_cell) {
+      let minX = 0;
+      let minY = 0;
+      let maxX = 7;
+      let maxY = 7; // probably good to have a method to establis grid bounds
+      let index = 0;
+
+      // console.log(a_cell.state);
+      if (a_cell.state === "hit") {
+        a_shooter.brain.potentialTargets.push([
+          a_cell.coords.x + 1,
+          a_cell.coords.y,
+        ]);
+        a_shooter.brain.potentialTargets.push([
+          a_cell.coords.x,
+          a_cell.coords.y + 1,
+        ]);
+        a_shooter.brain.potentialTargets.push([
+          a_cell.coords.x - 1,
+          a_cell.coords.y,
+        ]);
+        a_shooter.brain.potentialTargets.push([
+          a_cell.coords.x,
+          a_cell.coords.y - 1,
+        ]);
+      }
+
+      for (let target of a_shooter.brain.potentialTargets) {
+        if (
+          target[0] < minX ||
+          target[0] > maxX ||
+          target[1] < minY ||
+          target[1] > maxY
+        ) {
+          a_shooter.brain.potentialTargets.splice(index, 1);
+        }
+        index += 1;
+      }
+      // removes duplicate potential targets
+      let currentTargets = new Set(
+        a_shooter.brain.potentialTargets.map(JSON.stringify)
+      );
+      let uniquetargets = Array.from(currentTargets).map(JSON.parse);
+      console.log(uniquetargets);
+    },
+
     saveGame() {
       const game_to_save = {
         // creates a game object to hold current game state and populates game_to_save with current state
@@ -124,7 +177,6 @@ export default {
       GameService.addGame(game_to_save);
     },
     pullGame() {
-       
       GameService.getGame().then((result) => {
         // takes seed game at array index 0
         this.playerOne = result[0].game[4];
@@ -136,7 +188,6 @@ export default {
 
         this.playerTurn = this.playerOne.playerName;
         this.gameRunning = true;
-       
       });
     },
   },
