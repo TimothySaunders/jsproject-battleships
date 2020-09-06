c7276e9fd8fdf554ced1a12981c9b055f430fbcf
Timothy Saudners <timsaunders88@gmail.com>
2020-09-06 12:37:47 +0100

    changed gameRunning boolean to gameState string. all references updated

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <h3>{{ message }}</h3>
     <button v-on:click="saveGame">Save Game</button>
-    <game-grid :player="playerOne" :playerTurn="playerTurn" :gameState="gameRunning"></game-grid>
-    <game-grid :player="playerTwo" :playerTurn="playerTurn" :gameState="gameRunning"></game-grid>
+    <game-grid :player="playerOne" :playerTurn="playerTurn" :gameState="gameState"></game-grid>
+    <game-grid :player="playerTwo" :playerTurn="playerTurn" :gameState="gameState"></game-grid>
   </div>
 </template>
 
@@ -21,7 +21,7 @@ export default {
       // playerTurn: null,
       playerTurn: "Player 1",
       turns: 0,
-      gameRunning: false,
+      gameState: "",
       victor: "",
     };
   },
@@ -80,9 +80,9 @@ export default {
       // Sets the winner and ends the game if all ships of target are sunk
       this.checkIfAllSunk(target)
         ? ((this.victor = this.playerTurn),
-          (this.gameRunning = false),
+          (this.gameState = 'endGame'),
           (this.playerTurn = null))
-        : (this.gameRunning = true);
+        : (this.gameState = 'inGame');
     },
 
     checkIfAllSunk(player) {
@@ -110,11 +110,11 @@ export default {
         this.playerTwo = result[0].game[5];
         this.playerTurn = result[0].game[0].playerTurn;
         this.turns = result[0].game[1].turns;
-        this.gameRunning = result[0].game[2].gameRunning;
+        this.gameState = result[0].game[2].gameState;
         this.victor = result[0].game[3].victor;
 
         this.playerTurn = this.playerOne.playerName;
-        this.gameRunning = true;
+        this.gameState = 'inGame';
       });
     },
   },
@@ -128,7 +128,7 @@ export default {
   computed: {
     message: function () {
       // Provides feedback to the user describing current game state
-      return this.gameRunning
+      return this.gameState==='inGame'
         ? `${this.playerTurn}'s turn to Fire!`
         : `${this.victor} Wins!`;
     },
